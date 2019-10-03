322786055f8a27d1aa8d4333cfd91783b7758852
Victor Vilela Duarte <victorvduarte@gmail.com>
2019-10-03 01:31:50 -0300

    Tela de login e consulta a API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
+import api from './services/api';
 import './App.css';
+import logo from './assets/logo.svg';
 
 function App() {
+  const [email, setEmail] = useState('')
+
+  async function handleSubmit(event){
+    event.preventDefault();
+        
+    const response = await api.post('/sessions', { email });
+
+    const { _id } = response.data;
+
+    localStorage.setItem('user', _id);
+    // console.log(response);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="container">
+      <img src={logo} alt="AirCnC"/>
+      <div className="content">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">E-MAIL *</label>
+          <input type="email" id="email" placeholder="Seu melhor e-mail" value={email} onChange={event=> setEmail(event.target.value)}/>
+          <button className="btn" type="submit">ENTRAR</button>
+        </form>
+      </div>
     </div>
   );
 }
